4a2191b7d33b45209847c51b4a59115e7a2d3083
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-07-15 13:02:38 +0300

    added IncidentDashboardApplication.java

---
+++ src/main/java/com/oleksiidev/incidentdashboard/IncidentDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.oleksiidev.incidentdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentDashboardApplication.class, args);
+    }
+
+}
