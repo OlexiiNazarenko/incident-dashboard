09475987f74d76de5ae11ddb8e2059112f218d55
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-07-21 16:30:47 +0300

    refactored services

--- src/main/java/com/oleksiidev/incidentdashboard/services/IncidentService.java
+++ src/main/java/com/oleksiidev/incidentdashboard/services/IncidentService.java
@@ -2,24 +2,49 @@ package com.oleksiidev.incidentdashboard.services;
 
 import com.oleksiidev.incidentdashboard.dto.CreateIncidentDTO;
 import com.oleksiidev.incidentdashboard.model.Incident;
+import com.oleksiidev.incidentdashboard.model.IncidentStatus;
+import com.oleksiidev.incidentdashboard.repositories.IncidentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface IncidentService {
+@RequiredArgsConstructor
+@Service
+public class IncidentService {
 
-    Incident getIncidentById(Long id);
+    private final IncidentRepository incidentRepository;
 
-    List<Incident> getIncidentsByStatus(String statusName);
+    public Incident getIncidentById(Long id) {
+        return incidentRepository.findIncidentById(id);
+    }
 
-    List<Incident> getIncidentsByPlatformId(Long platformId);
+    public List<Incident> getIncidentsByStatus(String statusName) {
+        return incidentRepository.findIncidentsByStatus(IncidentStatus.valueOf(statusName));
+    }
 
-    List<Incident> getIncidentsByServiceId(Long serviceId);
+    public List<Incident> getIncidentsByPlatformId(Long platformId) {
+        return null;
+    }
 
-    List<Incident> getIncidentsByComponentId(Long componentId);
+    public List<Incident> getIncidentsByServiceId(Long serviceId) {
+        return null;
+    }
 
-    Incident createIncident(CreateIncidentDTO createIncidentDTO);
+    public List<Incident> getIncidentsByComponentId(Long componentId) {
+        return incidentRepository.findIncidentsByComponent_Id(componentId);
+    }
 
-    Incident updateIncident(Long id, CreateIncidentDTO createIncidentDTO);
+    public Incident createIncident(CreateIncidentDTO createIncidentDTO) {
+        Incident newIncident = new Incident();
+        return null;
+    }
 
-    void deleteIncident(Long id);
+    public Incident updateIncident(Long id, CreateIncidentDTO createIncidentDTO) {
+        return null;
+    }
+
+    public void deleteIncident(Long id) {
+
+    }
 }
--- src/main/java/com/oleksiidev/incidentdashboard/services/UserService.java
+++ src/main/java/com/oleksiidev/incidentdashboard/services/UserService.java
@@ -1,17 +1,50 @@
 package com.oleksiidev.incidentdashboard.services;
 
 import com.oleksiidev.incidentdashboard.dto.CreateUserDTO;
+import com.oleksiidev.incidentdashboard.model.Role;
 import com.oleksiidev.incidentdashboard.model.User;
+import com.oleksiidev.incidentdashboard.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
+import java.util.List;
+import java.util.NoSuchElementException;
 
-    User getUserById(Long id);
+@RequiredArgsConstructor
+@Service
+public class UserService {
 
-    User getUserByEmail(String email);
+    private final UserRepository userRepository;
 
-    User createUser(CreateUserDTO createUserDTO);
+    public User getUserById(Long id) {
+        return userRepository.findUserById(id);
+    }
 
-    User updateUser(Long id, CreateUserDTO createUserDTO);
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
 
-    void deleteUser(Long id);
+    public User getUserByEmail(String email) {
+        return userRepository.findUserByEmail(email);
+    }
+
+    public User createUser(CreateUserDTO createUserDTO) {
+        User newUser = new User();
+        newUser.setUsername(createUserDTO.getUsername());
+        newUser.setRole(Role.valueOf(createUserDTO.getRoleName()));
+        newUser.setEmail(createUserDTO.getEmail());
+        return userRepository.save(newUser);
+    }
+
+    public User updateUser(Long id, CreateUserDTO createUserDTO) throws NoSuchElementException {
+        User user = userRepository.findUserById(id);
+        user.setUsername(createUserDTO.getUsername());
+        user.setRole(Role.valueOf(createUserDTO.getRoleName()));
+        user.setEmail(createUserDTO.getEmail());
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
