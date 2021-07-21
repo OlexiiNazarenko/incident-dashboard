4395a6a9d87a60eafe3fd90e0236abffdc81795b
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-07-21 16:30:02 +0300

    refactored find by id methods in repositories

--- src/main/java/com/oleksiidev/incidentdashboard/repositories/IncidentRepository.java
+++ src/main/java/com/oleksiidev/incidentdashboard/repositories/IncidentRepository.java
@@ -4,18 +4,18 @@ import com.oleksiidev.incidentdashboard.model.Incident;
 import com.oleksiidev.incidentdashboard.model.IncidentStatus;
 import com.oleksiidev.incidentdashboard.model.IncidentType;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IncidentRepository extends CrudRepository<Incident, Long> {
 
-    Incident findById(long id);
+    Incident findIncidentById(long id);
 
     Incident findIncidentByType(IncidentType type);
 
     List<Incident> findIncidentsByStatus(IncidentStatus status);
 
     List<Incident> findIncidentsByComponent_Id(Long id);
-
-
 }
--- src/main/java/com/oleksiidev/incidentdashboard/repositories/UserRepository.java
+++ src/main/java/com/oleksiidev/incidentdashboard/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.oleksiidev.incidentdashboard.repositories;
 
 import com.oleksiidev.incidentdashboard.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    User findById(long id);
+    User findUserById(long id);
 
     User findUserByUsername(String username);
 
