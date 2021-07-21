236f66d9b3e88c78081d22b5daea2b3b25405956
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-07-21 14:09:38 +0300

    added column annotation in User

--- src/main/java/com/oleksiidev/incidentdashboard/model/User.java
+++ src/main/java/com/oleksiidev/incidentdashboard/model/User.java
@@ -2,22 +2,28 @@ package com.oleksiidev.incidentdashboard.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "User")
 public class User {
 
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
     private long id;
 
+    @Column
     private String username;
 
+    @Column
     private Role role;
 
+    @Column
     private String email;
 }
