f5ceb09c2afca01d6331880e71049cc8fb65a250
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-07-22 21:13:35 +0300

    removed findComponentsByRegions from ComponentRepository

--- src/main/java/com/oleksiidev/incidentdashboard/repositories/ComponentRepository.java
+++ src/main/java/com/oleksiidev/incidentdashboard/repositories/ComponentRepository.java
@@ -1,7 +1,6 @@
 package com.oleksiidev.incidentdashboard.repositories;
 
 import com.oleksiidev.incidentdashboard.model.Component;
-import com.oleksiidev.incidentdashboard.model.Region;
 import com.oleksiidev.incidentdashboard.model.Service;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -14,6 +13,4 @@ public interface ComponentRepository extends CrudRepository<Component, Long> {
     Component findComponentById(Long id);
 
     List<Component> findComponentsByService(Service service);
-
-    List<Component> findComponentsByRegions(List<Region> regions);
 }
