6770f6ad961e81742531d9c355cef87bac7a1aeb
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-07-22 20:19:00 +0300

    added SubscriptionController

--- src/main/java/com/oleksiidev/incidentdashboard/controllers/SubscriptionController.java
+++ src/main/java/com/oleksiidev/incidentdashboard/controllers/SubscriptionController.java
@@ -1,20 +1,26 @@
 package com.oleksiidev.incidentdashboard.controllers;
 
-import org.springframework.http.HttpStatus;
+import com.oleksiidev.incidentdashboard.dto.SubscriptionDTO;
+import com.oleksiidev.incidentdashboard.services.SubscriptionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class SubscriptionController {
 
+    private final SubscriptionService subscriptionService;
+
     @GetMapping("/subscribe")
-    public HttpStatus subscribe(@RequestParam String email) {
-        return HttpStatus.OK;
+    public boolean subscribe(@RequestBody SubscriptionDTO subscriptionDTO) {
+        return subscriptionService.createSubscription(subscriptionDTO);
     }
 
-    @GetMapping("/unsubscribe")
-    public HttpStatus unsubscribe(@RequestParam String email) {
-        return HttpStatus.OK;
+    @GetMapping("/unsubscribe/{email}")
+    public boolean unsubscribe(@PathVariable String email) {
+        return subscriptionService.deleteAllSubscriptionsForEmail(email);
     }
 }
