9452a7d1bd0b9e7a6bb0c548fd29411d2b04e56c
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-07-22 19:06:51 +0300

    Added subscription model

---
+++ src/main/java/com/oleksiidev/incidentdashboard/model/Subscription.java
@@ -0,0 +1,46 @@
+package com.oleksiidev.incidentdashboard.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "SUBSCRIPTION")
+public class Subscription {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "ID")
+    private long id;
+
+    @Column(name = "Email")
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "PlatformID")
+    private Platform platform;
+
+    @ManyToOne
+    @JoinColumn(name = "ServiceID")
+    private Service service;
+
+    @ManyToOne
+    @JoinColumn(name = "ComponentID")
+    private Component component;
+
+    @ManyToOne
+    @JoinColumn(name = "IncidentTypeID")
+    private IncidentType incidentType;
+
+    @ManyToOne
+    @JoinColumn(name = "RegionID")
+    private Region region;
+}
