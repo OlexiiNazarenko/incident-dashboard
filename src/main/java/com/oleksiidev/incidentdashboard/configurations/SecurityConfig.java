d91426534a3368e34092404027fa6e3f6f73549c
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-07-26 22:21:46 +0300

    added security configuration

---
+++ src/main/java/com/oleksiidev/incidentdashboard/configurations/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.oleksiidev.incidentdashboard.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/login").permitAll()
+                .antMatchers("/health").permitAll()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic().disable();
+
+        http.addFilterBefore(new JwtFilter(),
+                BasicAuthenticationFilter.class);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
