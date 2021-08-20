0a7dac8ef4745d0227d1ad0fbe5d9002a363a50f
oleksii-dev87 <oleksii.nazarenko@bazaarvoice.com>
2021-08-20 15:25:46 +0300

    added setters to Auth class

--- src/main/java/com/oleksiidev/incidentdashboard/configurations/AppProperties.java
+++ src/main/java/com/oleksiidev/incidentdashboard/configurations/AppProperties.java
@@ -14,6 +14,7 @@ public class AppProperties {
     private final OAuth2 oauth2 = new OAuth2();
 
     @Getter
+    @Setter
     public static class Auth {
         private String tokenSecret;
         private long tokenExpirationMsec;
